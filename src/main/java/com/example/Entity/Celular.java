4063cd48c407c85cbe0857ce3a78cda0ac99bf57
Saya <fl.daniela@javeriana.edu.co>
2024-05-21 16:50:56 -0500

    Back completo sin errores

--- src/main/java/com/example/Entity/Celular.java
+++ src/main/java/com/example/Entity/Celular.java
@@ -18,4 +18,76 @@ public class Celular {
     private String sistemaOperativo;
     private String gama;
     private boolean eliminado = false;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getSerial() {
+        return serial;
+    }
+
+    public void setSerial(String serial) {
+        this.serial = serial;
+    }
+
+    public LocalDate getFechaCompra() {
+        return fechaCompra;
+    }
+
+    public void setFechaCompra(LocalDate fechaCompra) {
+        this.fechaCompra = fechaCompra;
+    }
+
+    public int getAnoLanzamiento() {
+        return anoLanzamiento;
+    }
+
+    public void setAnoLanzamiento(int anoLanzamiento) {
+        this.anoLanzamiento = anoLanzamiento;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public String getSistemaOperativo() {
+        return sistemaOperativo;
+    }
+
+    public void setSistemaOperativo(String sistemaOperativo) {
+        this.sistemaOperativo = sistemaOperativo;
+    }
+
+    public String getGama() {
+        return gama;
+    }
+
+    public void setGama(String gama) {
+        this.gama = gama;
+    }
+
+    public boolean isEliminado() {
+        return eliminado;
+    }
+
+    public void setEliminado(boolean eliminado) {
+        this.eliminado = eliminado;
+    }
 }
